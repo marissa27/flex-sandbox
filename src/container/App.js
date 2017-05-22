167bfb7a795fca5dd9e8315d9abd85e8b0e4b3c1
Michael Fenwick <mickyfen17@aol.com>
2017-05-22 17:54:15 -0600

    add FlexParent Component and render via App

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from '../components/header';
+import { FlexParent } from '../components/flexParent';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <FlexParent />
       </div>
     );
   }
