ac93b0b2cf41e19de65d04681a4cd2c06b4597ac
Charles Dunn <dunnc1551@gmail.com>
2017-05-22 17:39:51 -0600

    Add header component and render button components with basic layout.

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from '../components/button';
+import { Header } from '../components/header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Flexbox Sandbox</h1>
-        <Button name="Show Code" className="btn show-code--btn"/>
-        <Button name="Reset" className="btn reset--btn"/>
+        <Header />
       </div>
     );
   }
