6640dbf6f0a7e127dbcf01455a7784d959c03162
Michael Fenwick <mickyfen17@aol.com>
2017-05-22 19:57:17 -0600

    add conditional to check first radio btn

--- src/components/subControl.js
+++ src/components/subControl.js
@@ -3,16 +3,19 @@ import React from 'react';
 export const SubControl = ({ properties, title, handleChange }) => {
 
   const renderRadioBtn = () => {
-    return properties.map((prop, i) =>
-            <p className="flex-prop" key={i}>
-              <input type="radio" id={ prop } value={ prop } name={ title }/>
-              <label htmlFor={ prop }>{ prop }</label>
-            </p>
-    );
+    return properties.map((prop, i) => {
+      const isCheck = i === 0 ? true : false;
+      return (
+        <p className="flex-prop" key={i}>
+          <input type="radio" id={ prop } value={ prop } name={ title } defaultChecked={ isCheck }/>
+          <label htmlFor={ prop }>{ prop }</label>
+        </p>
+      )
+    });
   }
 
   return (
-    <article>
+    <article className="control-list">
       <h3>{ title }</h3>
       { renderRadioBtn() }
     </article>
