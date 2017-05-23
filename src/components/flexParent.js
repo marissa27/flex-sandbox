d25280f6574c2823154acf4459591f88ded13e0d
Marissa Reinke <reinke.marissa@gmail.com>
2017-05-22 19:27:23 -0600

    Add state to flexParent and pass style properties to flexParent

--- src/components/flexParent.js
+++ src/components/flexParent.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlexChild } from './flexChild';
 
-export const FlexParent = ({ flexClass }) => {
-  const className = `flex-parent ${flexClass}`;
+export default class FlexParent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      display: "flex",
+      flexDirection: "",
+      flexWrap: "",
+      justifyContent: "",
+      alignItems: "",
+      alignContent: "",
+    }
+  }
 
-  const renderChildrenDiv = () => {
+
+  renderChildrenDiv = () => {
     return [1, 2, 3, 4].map((num, i) => {
       return (
         <FlexChild key={i} divNumber={ num }/>
       )
     })
   }
-
-  return (
-    <section className={ className }>
-      { renderChildrenDiv() }
-    </section>
-  )
+  render() {
+    return (
+      <section className="flex-parent" style={ this.state }>
+        { this.renderChildrenDiv() }
+      </section>
+    )
+  }
 }
--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from '../components/header';
-import { FlexParent } from '../components/flexParent';
+import FlexParent from '../components/flexParent';
 import { Controls } from '../components/controls';
 import { Footer } from '../components/footer';
 
