e8f0b489d7eb20579ba511cc2d5bf63bd28c5f8d
Charles Dunn <dunnc1551@gmail.com>
2017-05-23 13:33:11 -0600

    Change app to stateful component.

--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import { Header } from '../components/header';
-import FlexParent from '../components/flexParent';
+import { FlexParent } from '../components/flexParent';
 import { Controls } from '../components/controls';
 import { Footer } from '../components/footer';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      display: "flex",
+      flexDirection: "",
+      flexWrap: "",
+      justifyContent: "",
+      alignItems: "",
+      alignContent: "",
+    }
+  }
+
+  formatTitle(title) {
+    let splitTitle = title.split('-');
+    splitTitle = splitTitle[0] + splitTitle[1].slice(0,1).toUpperCase() + splitTitle[1].slice(1);
+    return splitTitle;
+  }
+
+  handleChange(title, prop) {
+    let formattedTitle = this.formatTitle(title);
+    this.setState({
+      [formattedTitle]: prop
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <FlexParent />
-        <Controls />
+        <FlexParent style={this.state}/>
+        <Controls handleChange={this.handleChange.bind(this)}/>
         <Footer />
       </div>
     );
