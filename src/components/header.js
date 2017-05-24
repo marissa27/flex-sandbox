03d27f4e24ac0406e8aa5b770ecaddf432c41850
Michael Fenwick <mickyfen17@aol.com>
2017-05-23 23:07:34 -0600

    pass toggle func for ShowCode to button

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button } from './button';
 
-export const Header = ({ handleDiv, handleReset }) => {
+export const Header = ({ handleDiv, handleReset, handleShowCode }) => {
   return (
     <header>
       <h1>Flexbox Sandbox</h1>
       <Button name="Add Div" className="btn add-div--btn" handleClick={ handleDiv }/>
-      <Button name="Show Code" className="btn show-code--btn"/>
+      <Button name="Show Code" className="btn show-code--btn" handleClick={ handleShowCode }/>
       <Button name="Reset" className="btn reset--btn" handleClick={ handleReset }/>
     </header>
   )
