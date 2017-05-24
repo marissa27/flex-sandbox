b9740f6217ad8225dc193cce3b351c58abd5912a
Michael Fenwick <mickyfen17@aol.com>
2017-05-23 23:08:11 -0600

    add ShowCode component and basic styling

---
+++ src/components/showCode.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './showCode.css';
+
+export const ShowCode = () => {
+  return (
+    <section className="show-code--section">
+      <h1>Show Code</h1>
+    </section>
+  )
+}
