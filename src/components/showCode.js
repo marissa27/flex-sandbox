1456f1b907998454bc90b3e7066e833f0406bbc9
Michael Fenwick <mickyfen17@aol.com>
2017-05-24 00:06:31 -0600

    render divs as html code

--- src/components/showCode.js
+++ src/components/showCode.js
@@ -3,11 +3,35 @@ import { Button } from './button';
 
 import './showCode.css';
 
-export const ShowCode = ({ handleShowCode }) => {
+export const ShowCode = ({ handleShowCode, divs }) => {
+
+  const toHTML = () => {
+    return divs.map(div => {
+      return `
+        <div id="flexbox-child ${div}"></div>
+      `
+    }).join('')
+  }
+
+  const toRender = () => {
+    return (
+      <code>
+        {`
+          <section class="flexbox-container">
+            ${ toHTML() }
+          </section>
+        `}
+      </code>
+    )
+  }
+
   return (
     <section className="show-code--section">
       <Button name="X" className="btn toggle--btn" handleClick={ handleShowCode }/>
-      <h1>Show Code</h1>
+      <h1 className="show-code-header">Source Code</h1>
+      <div className="html-div">
+        { toRender() }
+      </div>
     </section>
   )
 }
