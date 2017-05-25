a1298d139d0f6dbd12b3c9c14bb0991bea13336b
Michael Fenwick <mickyfen17@aol.com>
2017-05-25 11:09:01 -0600

    fix file path for css

--- src/components/showCode.js
+++ src/components/showCode.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from './button';
 
-import '../styles/showCode.css';
+import './showCode.css';
 
 export const ShowCode = ({ handleShowCode, divs, style }) => {
 
